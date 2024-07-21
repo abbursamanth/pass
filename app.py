48abee02c704f51472babe9c6afa48ab4c2cd901
abbursamanth <142981101+abbursamanth@users.noreply.github.com>
2024-07-21 17:39:49 +0530

    2 update app.py

--- app.py
+++ app.py
@@ -4,12 +4,17 @@ import string
 import streamlit as st
 
 
+
+
+
+
 def brute_force(password):
     start_time = time.time()
     characters = string.ascii_letters + string.digits + string.punctuation
     for length in range(1, len(password) + 1):
         for guess in itertools.product(characters, repeat=length):
             guess = ''.join(guess)
+            print(f"Brute Force trying: {guess}")
             if guess == password:
                 end_time = time.time()
                 return guess, end_time - start_time
@@ -18,7 +23,9 @@ def brute_force_time_complexity(password_length):
     return "O(c^n)"  # where c is the number of possible characters, and n is the password length
 
 
+
 def backtracking(password, guess='', characters=string.ascii_letters + string.digits + string.punctuation):
+    print(f"Backtracking trying: {guess}")
     if len(guess) == len(password):
         if guess == password:
             return guess
@@ -37,6 +44,7 @@ def backtracking_password(password):
 
 def backtracking_time_complexity(password_length):
     return "O(n * c^n)"  # where n is the password length and c is the number of possible characters
+
 
 
 
